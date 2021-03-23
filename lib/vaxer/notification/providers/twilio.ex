61b43c051f704cbe8d61b2fc9b513e1885933c84
Joe Sweeney <joe@j1.io>
2021-03-23 15:50:04 -0400

    Send SMS messages via Twilio

--- lib/vaxer/notification/providers/twilio.ex
+++ lib/vaxer/notification/providers/twilio.ex
@@ -13,7 +13,7 @@ defmodule Vaxer.Notification.Providers.Twilio do
   def init([twilio_phone_number: twilio_phone_number, notification_phone_numbers: notification_phone_numbers]) do
     Logger.info("Starting #{@prefix} with phone number #{twilio_phone_number} and notification phone numbers: #{Enum.join(notification_phone_numbers, ", ")}...")
 
-    {:ok, %{twilio_phone_number: twilio_phone_number}}
+    {:ok, %{twilio_phone_number: twilio_phone_number, notification_phone_numbers: notification_phone_numbers}}
   end
 
   def notify(source) do
@@ -21,8 +21,14 @@ defmodule Vaxer.Notification.Providers.Twilio do
   end
 
   @impl true
-  def handle_cast({:notify, source}, %{twilio_phone_number: twilio_phone_number} = state) do
+  def handle_cast({:notify, source}, %{twilio_phone_number: twilio_phone_number, notification_phone_numbers: notification_phone_numbers} = state) do
     Logger.info("#{@prefix} notifying about #{source}...")
+
+    Enum.each(notification_phone_numbers, fn target_number ->
+      body = "Found vaccines at #{source}"
+
+      ExTwilio.Message.create(to: target_number, from: twilio_phone_number, body: body)
+    end)
 
     {:noreply, state}
   end
