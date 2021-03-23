a96409321df3551e79e77753bcdbabf3e3051706
Joe Sweeney <joe@j1.io>
2021-03-23 13:56:18 -0400

    Get vaccine info for CVS provider

--- lib/vaxer/web/providers/cvs.ex
+++ lib/vaxer/web/providers/cvs.ex
@@ -1,8 +1,11 @@
 defmodule Vaxer.Web.Providers.CVS do
   use GenServer
   require Logger
-
   alias Wallaby
+  alias Wallaby.{Browser, Query, Element}
+
+  @url "https://www.cvs.com/immunizations/covid-19-vaccine"
+  @prefix "CVS provider"
 
   def start_link([]) do
     GenServer.start_link(__MODULE__, [], [])
@@ -16,5 +19,30 @@ defmodule Vaxer.Web.Providers.CVS do
     timer_ref = Process.send_after(self(), :check, 1000)
 
     {:ok, %{session: session, timer: timer_ref}}
+  end
+
+  @impl true
+  def handle_info(:check, %{session: session} = state) do
+    Logger.debug("#{@prefix} checking...")
+
+    result = check(session)
+    if result do
+      Logger.info("#{@prefix} found vaccines!")
+    else
+      Logger.debug("#{@prefix} did not find any vaccines")
+    end
+
+    {:noreply, state}
+  end
+
+  defp check(session) do
+    session
+    |> Browser.visit(@url)
+    |> Browser.click(Query.link("Massachusetts"))
+    |> Browser.all(Query.css("span.status"))
+    |> Enum.any?(fn element ->
+      text = Element.text(element)
+      !String.contains?(text, "Fully Booked")
+    end)
   end
 end
