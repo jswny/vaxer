cbee4b95c132bf75bceff348d3121d2f58511de9
Joe Sweeney <joe@j1.io>
2021-03-23 15:00:41 -0400

    Fix config for delay env var

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,7 +2,7 @@ import Config
 
 app_name = :vaxer
 
-get_env_var = fn var_name, default ->
+get_env_var = fn var_name, type, default ->
   value = System.get_env(var_name)
 
   if value == nil || value == "" do
@@ -14,9 +14,18 @@ get_env_var = fn var_name, default ->
       """
     end
   else
-    value
+    case type do
+      :int ->
+        {value, _} = Integer.parse(value)
+        value
+      _ -> value
+    end
   end
 end
 
 config :vaxer,
-  delay: get_env_var.("DELAY", 10000)
+  delay: get_env_var.("DELAY", :int, 10000)
+
+config :ex_twilio,
+  account_sid: get_env_var.("TWILIO_ACCOUNT_SID", nil, nil),
+  auth_token: get_env_var.("TWILIO_AUTH_TOKEN", nil, nil)
