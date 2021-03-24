e58f30970ca72910b2a3b809e420f2b49a8c7bae
Joe Sweeney <joe@j1.io>
2021-03-23 22:21:35 -0400

    Create new session in each check

--- lib/vaxer/web/providers/cvs.ex
+++ lib/vaxer/web/providers/cvs.ex
@@ -16,25 +16,29 @@ defmodule Vaxer.Web.Providers.CVS do
   def init([selenium_url: selenium_url, delay: delay, state_abbreviation: state_abbreviation]) do
     Logger.info("Starting #{@prefix} for state #{state_abbreviation} with delay #{delay} ms...")
 
-    {:ok, session} =
-      if selenium_url != nil do
-        Wallaby.start_session(remote_url: selenium_url, capabilities: %{browserName: "chrome"})
-      else
-        Wallaby.start_session()
-      end
+    one_minute = 60000
 
-    timer = create_check_timer(60000)
+    initial_check_delay =
+      if delay < one_minute do
+        delay
+      else
+        one_minute
+      end
+    timer = create_check_timer(initial_check_delay)
 
     {:ok, state_name} = Vaxer.Location.get_state_name_from_abbreviation(state_abbreviation)
 
-    {:ok, %{session: session, timer: timer, delay: delay, state_name: state_name}}
+    {:ok, %{selenium_url: selenium_url, timer: timer, delay: delay, state_name: state_name}}
   end
 
   @impl true
-  def handle_info(:check, %{session: session, delay: delay, state_name: state_name} = state) do
+  def handle_info(:check, %{selenium_url: selenium_url, delay: delay, state_name: state_name} = state) do
     Logger.debug("#{@prefix} checking...")
 
+    session = start_session(selenium_url)
     result = check(session, state_name)
+    Wallaby.end_session(session)
+
     if result do
       Logger.info("#{@prefix} found vaccines!")
       Twilio.notify("CVS", @url)
@@ -50,12 +54,23 @@ defmodule Vaxer.Web.Providers.CVS do
   end
 
   @impl true
-  def terminate(_reason, %{session: session}) do
-    Wallaby.end_session(session)
+  def terminate(_reason, %{timer: timer}) do
+    Process.cancel_timer(timer)
   end
 
   defp create_check_timer(delay) do
     Process.send_after(self(), :check, delay)
+  end
+
+  defp start_session(selenium_url) do
+    {:ok, session} =
+      if selenium_url != nil do
+        Wallaby.start_session(remote_url: selenium_url, capabilities: %{browserName: "chrome"})
+      else
+        Wallaby.start_session()
+      end
+
+    session
   end
 
   defp check(session, state_name) do
